fc0df0e5f6b7dbeed5e334e55da36c5dc568e8c8
radolini3 <radolini3@o2.pl>
2022-11-24 11:50:20 +0100

    Update - do zrobienia wyrkesy

--- dataCapture/serialMonitor.py
+++ dataCapture/serialMonitor.py
@@ -1,3 +1,4 @@
+from curses import raw
 from datetime import datetime
 from socket import timeout
 import serial
@@ -8,10 +9,6 @@ import json
 import glob
 import sys
 import time 
-
-# def sendSignal():
-#     time.sleep(1)
-#     serialPort.write("W\r".encode())
 
 # Zapisz historię pomiarów do pliku csv
 def save_to_csv(tab):
@@ -31,7 +28,18 @@ def save_to_csv(tab):
     file.close()
 #===================================================================================================================================
 #Zapisz do JSONa  - wartości z ostatniego pobrania wartości z czujników 
-def JSONdumpster(arr):
+
+def JSONconfig(config):
+    config = {
+    "Threshold": config[1],
+    "Mode": config[2],
+    "IrrigTime": config[3]
+    }
+    config = json.dumps(config, indent = 1)
+    with open("dataCapture/config.json", "w") as outfile_2:
+        outfile_2.write(config)
+
+def JSONsensors(arr):
     arr = {
     "Temperature": arr[0],
     "AirHumidity": arr[1],
@@ -43,9 +51,9 @@ def JSONdumpster(arr):
     "SoilSensor6": arr[7]
     }
     json_obj = json.dumps(arr, indent = 1)
-
     with open("dataCapture/sensors.json", "w") as outfile:
         outfile.write(json_obj)
+
 #===================================================================================================================================
 #Sprawdź platformę na której skrypt działa, potem sprawdź dostępne porty i zwróć użytkownikowi
 def serial_ports():
@@ -55,8 +63,7 @@ def serial_ports():
         ports = ['COM%s' % (i + 1) for i in range(256)]
 
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
+        ports = glob.glob('/dev/tty[A-Za-z]*')        # this excludes your current terminal "/dev/tty"
 
     elif sys.platform.startswith('darwin'):
         ports = glob.glob('/dev/tty.*')
@@ -86,6 +93,32 @@ def detectPort():
     #Ponumerowana lista portów
     for i in range (0, len(serialPortList)):
         print(str(i+1) + ". " + str(serialPortList[i]))
+    print("Detected port: " + serialPortList[0])
+    return serialPortList[0]
+
+print("Detected OS: " + sys.platform)
+port = detectPort()
+serialPort = serial.Serial(port,baudrate=38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=None) #timeout=None każe czekać nonstop na przychodzące dane, jeśli damy jakąś stałą, to po nie otrzymaniu danych w zadanym czasie zwróci błąd potem.
+print("Running - Do not close this terminal in order to mantain capturing data")
+#Nieskończona pętla monitorująca port szeregowy w poszukiwaniu przychodzących danych
+while True:
+        while serialPort.in_waiting:
+            strUART = serialPort.readline()
+            rawData = strUART.decode('utf8')
+            rawData = rawData.strip('\n')
+            if rawData.startswith("C"):
+                config = rawData.split("/")
+                print(config)
+                JSONconfig(config)
+                continue
+            sensors = rawData.split("/")
+            sensors.pop(0)
+            print(sensors)
+            JSONsensors(sensors)
+            save_to_csv(sensors)
+
+
+
 #===================================================================================================================================
     # listSize = len(serialPortList)
     # print("Select port which is connected to microcontroller by entering number: ")
@@ -99,21 +132,3 @@ def detectPort():
     #         print("Enter only digit / Value entered is out of range")
     #         continue
 #===================================================================================================================================
-    print("Detected port: " + serialPortList[0])
-    return serialPortList[0]
-
-print("Detected OS: " + sys.platform)
-port = detectPort()
-serialPort = serial.Serial(port,baudrate=38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=None) #timeout=None każe czekać nonstop na przychodzące dane, jeśli damy jakąś stałą, to po nie otrzymaniu danych w zadanym czasie zwróci błąd potem.
-print("Running - Do not close this terminal in order to mantain capturing data")
-#Nieskończona pętla monitorująca port szeregowy w poszukiwaniu przychodzących danych
-while True:
-        while serialPort.in_waiting:
-            strUART = serialPort.readline()
-            rawSensorData = strUART.decode('utf8')
-            rawSensorData = rawSensorData.strip('\n')
-            sensors = rawSensorData.split("S")
-            sensors.pop(0)
-            print(sensors)
-            JSONdumpster(sensors)
-            save_to_csv(sensors)
--- server.py
+++ server.py
@@ -10,18 +10,55 @@ import csv
 
 app = Flask(__name__)
 
+#Dodanie ścieżki do skryptów pythona 
 sys.path.insert(0, '/dataCapture')
 
 @app.route("/") 
 def home():
+    #Otwórz plik sensors.json 
     with open('./dataCapture/sensors.json', 'r') as myfile:
         data = json.load(myfile)
-        sensors = [data['Temperature'], data['AirHumidity']]
-        SoilSensors = [int(data['SoilSensor1']), int(data['SoilSensor2']), int(data['SoilSensor3']), int(data['SoilSensor4']), int(data['SoilSensor5']), int(data['SoilSensor6'])]
-        average = int(sum(SoilSensors)/len(SoilSensors))
-    return render_template("home.html", avg = average, airTemp = sensors[0], airH = sensors[1], SoilH1 = SoilSensors[0], SoilH2 = SoilSensors[1], SoilH3 = SoilSensors[2], SoilH4 = SoilSensors[3], SoilH5 = SoilSensors[4], SoilH6 = SoilSensors[5])
+        #Pobranie odczytów z JSON'a 
+        sensors = [
+          data['Temperature'],
+          data['AirHumidity']]
+        SoilSensors = [
+         int(data['SoilSensor1']),
+         int(data['SoilSensor2']),
+         int(data['SoilSensor3']), 
+         int(data['SoilSensor4']),
+         int(data['SoilSensor5']),
+         int(data['SoilSensor6'])]
+        average = int(sum(SoilSensors)/len(SoilSensors)) # Średnia z ponmiarów czujników wilgotności
 
-@app.route("/charts")	
+    with open('./dataCapture/config.json', 'r') as myfile2:
+        data = json.load(myfile2)
+        config = [
+         data['Threshold'], 
+         data['Mode'],
+         data['IrrigTime']]
+
+        if config[1] == '1':
+            Mode = "Automatyczny"
+        else:
+            Mode = "Manualny"
+
+    return render_template(
+     "home.html",
+     mode = Mode,            # tryb podlewania z mikrokontrolera
+     Threshold = config[0],  # próg podlewania przy trybie automatycznym 
+     irrigtime = config[2],
+     avg = average,          # Srednia wilgotności gleby 
+     airTemp = sensors[0],   # temperatura powietrza
+     airH = sensors[1],      # Wilgotność powietrza 
+     SoilH1 = SoilSensors[0],
+     SoilH2 = SoilSensors[1],
+     SoilH3 = SoilSensors[2],
+     SoilH4 = SoilSensors[3],
+     SoilH5 = SoilSensors[4],
+     SoilH6 = SoilSensors[5])
+
+@app.route("/charts")	# Wykres średniej pomiarów od czasu (dzisiejsze pomiary tylko atm)
 def charts():
     with open('./dataCapture/pomiary.csv', 'r') as csvfile:
         data = list(csv.reader(csvfile, delimiter=','))
@@ -32,11 +69,11 @@ def charts():
         print(todayData)
     return render_template("charts.html")
     
-@app.route("/about")	
+@app.route("/about")	# kilka słów o projekcie 
 def about():
     return render_template("about.html")
 
-@app.route("/settings")	
+@app.route("/settings")	    # Ustawienia (narazie do usunięcia )
 def settings():
     return render_template("settings.html")
 
